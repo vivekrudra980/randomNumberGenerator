afb42819cc097f550bd54090a457d371343fce43
vivek <vivekprudra@gamail.com>
2023-08-10 08:39:44 +0000

    random number generator

---
+++ components/EvenOddApp/index.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  onIncrement = () => {
+    const randomNumber = Math.ceil(Math.random() * 100)
+    console.log(randomNumber)
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const text = count % 2 === 0 ? 'Even' : 'Odd'
+
+    return (
+      <div className="app-container">
+        <div className="container">
+          <h1 className="heading">Count {count}</h1>
+          <p>Count is {text}</p>
+          <button type="button" className="button" onClick={this.onIncrement}>
+            Increment
+          </button>
+          <p>*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
